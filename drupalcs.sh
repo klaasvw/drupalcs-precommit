d40f235824a39f420ed2607aadf1dcd10426ee77
klaasvw <klaasvw@gmail.com>
2012-09-26 16:21:12 +0200

    Added whitelist for only checking PHP code.

--- drupalcs.sh
+++ drupalcs.sh
@@ -8,16 +8,37 @@
 # - drush <http://drupal.org/project/drush>
 # - drupalcs <http://drupal.org/project/drupalcs>
 
+DRUPALCS_FILE_PATTERN="\.(php|module|inc|install|test|profile|theme)$"
+
+# Initial check to see if all requirements to run this script are met.
+# Check if drush exists and is executable.
+if ! command -v drush >/dev/null 2>&1; then
+  echo "Drush bin not found or executable."
+  exit 1
+fi
+# Check if the drupal code sniffer drush plugin is available.
+if ! drush dcs -hq; then
+  echo "The drush drupalcs module has not been installed."
+  exit 1
+fi
+
 # Stash the current changes so only the staged code is evaluated.
 git stash -q --keep-index
 # Exit when one check fails.
-exit_status=0
-# Check each file in the staging area
-for result in $(git diff --cached --name-only --diff-filter=AM); do
-  drush dcs --extensions=php,module,inc,install,test,profile,theme $result
-  [ $? -ne 0 ] && exit_status=1
+RETVAL=0
+# Collect the output.
+OUTPUT=""
+# Check each file in the staging area.
+for FILE in $(git diff --cached --name-only --diff-filter=ACRM); do
+  # Check against whitelist.
+  echo $FILE | egrep -q $DRUPALCS_FILE_PATTERN
+  if [ $? -eq 0 ]; then
+    drush dcs $FILE;
+    [ $? -ne 0 ] && RETVAL=1
+  fi;
 done
 # Unstash the changes to go back to the current working tree.
 git stash pop -q
 
-exit $exit_status
+exit 1;
+#exit $RETVAL
